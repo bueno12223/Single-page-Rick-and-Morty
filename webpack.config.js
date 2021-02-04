496ab688c511b74b86a3eb22ae00717fe14f6045
Jesus <berriojesus122@gmail.com>
2021-02-04 16:13:26 -0500

    estructura HTML lista

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,36 @@
-const path = require("path")
-const htmlWebpackPlugin = require("html-webpack-plugin"); 
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports ={
-    entry: "./src/index.js",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "main.js"
-    },
-    resolve: {
-        extensions: [".js"]
-    },
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+  module: {
     rules: [
-        {
-            test: /\.js?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',    
-            }
-        ],
-        plugins: [ 
-            new HtmlWebpackPlugin([
-                {
-                    inject: true,  
-                    template: './public/index.html',   
-                    filename: './index.html'   
-                }
-            ])
-        ]
-    }
-    
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin([
+      {
+        inject: true,
+        template: './public/index.html',
+        filename: './index.html',
+      }
+    ]),
+  ]
+}
 
 
    
